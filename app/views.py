ab09bc3034f61a1833e60e04ef1022a0e5eaef55
Mukkamsravani <mukkamsravani143@gmail.com>
2023-05-20 17:08:56 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def srujana(request):
+    return HttpResponse('<marquee><h1>srujana thinnavara era vadilesthunnavara</marquee></h1>')
